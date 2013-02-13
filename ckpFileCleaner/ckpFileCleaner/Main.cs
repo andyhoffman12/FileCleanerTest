92b7a42a1c20a5e76eb92c323b69119c077bde20
Andrew Hoffman <andy.hoffman12@gmail.com>
2013-02-13 16:32:03 -0600

    Renamed Files

--- ckpFileCleaner/ckpFileCleaner/Main.cs
+++ ckpFileCleaner/ckpFileCleaner/Main.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    static class Program
+    static class Main
     {
         /// <summary>
         /// The main entry point for the application.
