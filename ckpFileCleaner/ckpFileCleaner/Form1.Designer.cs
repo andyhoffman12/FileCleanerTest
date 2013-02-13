08dc8544ef3ecd9242f6503fe35353e126932444
Andrew Hoffman <andy.hoffman12@gmail.com>
2013-02-13 15:29:18 -0600

    First Revision

---
+++ ckpFileCleaner/ckpFileCleaner/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonExecuteProcess = new System.Windows.Forms.Button();
+            this.labelFileSelected = new System.Windows.Forms.Label();
+            this.labelOutputFile = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(30, 29);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonExecuteProcess
+            // 
+            this.buttonExecuteProcess.Location = new System.Drawing.Point(152, 28);
+            this.buttonExecuteProcess.Name = "buttonExecuteProcess";
+            this.buttonExecuteProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecuteProcess.TabIndex = 1;
+            this.buttonExecuteProcess.Text = "Process";
+            this.buttonExecuteProcess.UseVisualStyleBackColor = true;
+            this.buttonExecuteProcess.Click += new System.EventHandler(this.buttonExecuteProcess_Click);
+            // 
+            // labelFileSelected
+            // 
+            this.labelFileSelected.AutoSize = true;
+            this.labelFileSelected.Location = new System.Drawing.Point(30, 74);
+            this.labelFileSelected.Name = "labelFileSelected";
+            this.labelFileSelected.Size = new System.Drawing.Size(85, 13);
+            this.labelFileSelected.TabIndex = 2;
+            this.labelFileSelected.Text = "No File Selected";
+            this.labelFileSelected.Click += new System.EventHandler(this.labelFileSelected_Click);
+            // 
+            // labelOutputFile
+            // 
+            this.labelOutputFile.AutoSize = true;
+            this.labelOutputFile.Location = new System.Drawing.Point(30, 111);
+            this.labelOutputFile.Name = "labelOutputFile";
+            this.labelOutputFile.Size = new System.Drawing.Size(85, 13);
+            this.labelOutputFile.TabIndex = 3;
+            this.labelOutputFile.Text = "No File Selected";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "ckp Files|*.ckp|All Files|*.*";
+            this.openFileDialog1.Title = "Open File with Nulls";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.labelOutputFile);
+            this.Controls.Add(this.labelFileSelected);
+            this.Controls.Add(this.buttonExecuteProcess);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "MainForm";
+            this.Text = "ckp file cleaner v1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Button buttonExecuteProcess;
+        private System.Windows.Forms.Label labelFileSelected;
+        private System.Windows.Forms.Label labelOutputFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ckpFileCleaner/ckpFileCleaner/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    
+
+    public partial class MainForm : Form
+    {
+        public string ChosenFile = "";
+        public string file = "";
+        public string result = "";
+        string[] removeArray = new string[] { "	:obj_name ", "			:Table (network_objects)\r\n", "	:obj_table (network_objects)\r\n", "		:update_url (\"network_objects:\")\r\n", "		:update_logic (override)\r\n", "		:update_id_param (@)\r\n", "			:ClassName (host_plain)\r\n", "		:type (host)\r\n", "			:table (network_objects)\r\n", "		:Wiznum (-1)\r\n", "			:icon (\"NetworkObjects/Nodes/Host\")\r\n", "		:read_community ()\r\n", "		:sysContact ()\r\n", "		:sysDescr ()\r\n", "		:sysLocation ()\r\n", "		:sysName ()\r\n", "		:write_community ()\r\n", "		:add_adtr_rule (false)\r\n", "		:certificates ()\r\n", "		:data_source (not-installed)\r\n", "		:edges ()\r\n", "		:enforce_gtp_rate_limit (false)\r\n", "		:gtp_rate_limit (2048)\r\n", "		:interfaces ()\r\n", "		:os_info ()\r\n", "		:DAG (false)\r\n", "		:NAT ()\r\n", "		:SNMP ()\r\n", "		:VPN ()\r\n", "		:additional_products ()\r\n", "		:color (black)\r\n", "		:comments ()\r\n", "		:connectra (false)\r\n", "		:connectra_settings ()\r\n", "		:cp_products_installed (false)\r\n", "		:data_source_settings ()\r\n", "		:firewall (not-installed)\r\n", "		:floodgate (not-installed)" };
+        int stringIndex = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            ChosenFile = openFileDialog1.FileName;
+            labelFileSelected.Text = ChosenFile;
+            labelFileSelected.TextChanged +=new System.EventHandler(this.labelFileSelected_TextChanged);
+            StreamReader streamReader = new StreamReader(ChosenFile);
+            file = streamReader.ReadToEnd();
+            streamReader.Close();
+        }
+
+        private void buttonExecuteProcess_Click(object sender, EventArgs e)
+        {
+            result = file.Replace("\0", string.Empty);
+
+            foreach (string i in removeArray)
+            {
+                result = result.Replace(i, String.Empty);
+            }
+            result = result.Replace("(", string.Empty);
+            result = result.Replace(")", string.Empty);
+            
+            stringIndex = result.IndexOf("./export_details");
+            result = result.Remove(0, stringIndex);
+
+            stringIndex = result.IndexOf("mkgroup");
+            result = result.Remove(0, stringIndex);
+
+            result = result.Replace("mkgroup\r\n", string.Empty);
+
+            stringIndex = result.IndexOf("servers-no-internet");
+            result = result.Remove(stringIndex);
+
+            StreamWriter streamWriter = new StreamWriter(ChosenFile + ".txt");
+            streamWriter.Write(result);
+            streamWriter.Close();
+            labelOutputFile.Text = ChosenFile + ".txt";
+            labelOutputFile.TextChanged += new System.EventHandler(this.labelOutputFile_TextChanged);
+
+        }
+
+        private void labelFileSelected_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void labelFileSelected_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void labelOutputFile_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
